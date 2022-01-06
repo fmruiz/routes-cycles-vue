14e5a7f2c3df6ec4ef4d6c1483350600945f209f
franco <x.francoruiz@gmail.com>
2022-01-05 23:15:51 -0300

    add not page found

--- src/router/router.js
+++ src/router/router.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import ListPage from "../modules/pokemon/pages/ListPage.vue";
 import AboutPage from "../modules/pokemon/pages/AboutPage.vue";
 import PokemonPage from "../modules/pokemon/pages/PokemonPage.vue";
+import NotPageFound from "../modules/shared/pages/NotPageFound.vue";
 
 const routes = [
   { path: "/", component: ListPage },
   { path: "/about", component: AboutPage },
   { path: "/id", component: PokemonPage },
+  { path: "/:pathMatch(.*)*", component: NotPageFound },
 ];
 
 const router = createRouter({
@@ -14,4 +16,4 @@ const router = createRouter({
   routes,
 });
 
-export default router
+export default router;
