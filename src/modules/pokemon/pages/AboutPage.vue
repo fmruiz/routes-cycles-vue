838cc7e96df301271c89b737bdd0f9dd83340753
franco <x.francoruiz@gmail.com>
2022-01-08 21:14:06 -0300

    add lifecycles hooks

--- src/modules/pokemon/pages/AboutPage.vue
+++ src/modules/pokemon/pages/AboutPage.vue
@@ -3,7 +3,47 @@
 </template>
 
 <script>
-export default {};
+export default {
+  beforeCreate() {
+    console.log("beforeCreate");
+  },
+  created() {
+    console.log("created");
+  },
+  beforeMount() {
+    console.log("beforeMount");
+  },
+  mounted() {
+    console.log("mounted");
+  },
+  beforeUpdate() {
+    console.log("beforeUpdate");
+  },
+  updated() {
+    console.log("updated");
+  },
+  activated() {
+    console.log("activated");
+  },
+  deactivated() {
+    console.log("deactivated");
+  },
+  beforeUnmount() {
+    console.log("beforeUnmount");
+  },
+  unmounted() {
+    console.log("unmounted");
+  },
+  errorCaptured() {
+    console.log("errorCaptured");
+  },
+  renderTracked() {
+    console.log("renderTracked");
+  },
+  renderTriggered() {
+    console.log("renderTriggered");
+  },
+};
 </script>
 
 <style></style>
